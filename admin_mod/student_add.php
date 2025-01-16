b10131d6797cfc3c735d16f96fee61cce03679a4
Pavan123953 <72126236+Pavan123953@users.noreply.github.com>
2025-01-16 12:01:49 -0600

    Add files via upload

---
+++ admin_mod/student_add.php
@@ -0,0 +1,72 @@
+<?php include '../assets/includes/header.php'; ?>
+
+<!-- DIRECTORY -->
+<div class="main-dir">
+	<h1>Classes</h1>
+</div>
+<!-- END DIRECTORY -->
+
+<!-- CONTAINER -->
+<div class="main-container">
+	<div class="inner-container">
+		<div class="content-row">
+			<div class="panel back-color-white">
+				<div class="panel-header-norm">
+					<h2>New Student</h2>
+				</div>
+				<div class="panel-content-norm">
+					<span class="error" id="error"></span>
+					<form class="enter-data" method="post" id="setStudent">
+						<div>
+							<label class="data-name">Student ID:</label>
+							<input type="text" id="studentID" required>
+						</div>
+						<div>
+							<label class="data-name">First Name:</label>
+							<input type="text" id="firstName" required>
+						</div>
+						<div>
+							<label class="data-name">Last Name:</label>
+							<input type="text" id="lastName" required>
+						</div>
+						<div>
+							<label class="data-name">Mobile:</label>
+							<input type="tel" id="mobile" pattern="[0-9]+" maxlength="11" required>
+						</div>
+						<div>
+							<label class="data-name">Date of birth:</label>
+							<input type="date" id="dob" required>
+						</div>
+						<div>
+							<label class="data-name">Course:</label>
+							<input type="text" id="course" required>
+						</div>
+						<div class="register-form-group">
+							<label>Program</label>
+							<select name="program" class="form-input form-input-dropdwon" id="program" required>
+								<option value="Graduate">Graduate</option>
+								<option value="Undergraduate">Undergraduate</option>
+								<option value="Other">Other</option>
+							</select>
+						</div>
+						<div>
+							<label class="data-name">Password:</label>
+							<input type="password" id="password" required>
+						</div>
+						<div>
+							<label class="data-name">Confirm Password:</label>
+							<input type="password" id="confirmPassword" required>
+						</div>
+						<div class="col-btn">
+							<button class="button back-color-green">Create</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END CONTAINER -->
+<script src="../assets/javascript/elearning.scripts.js?<?php echo time(); ?>"></script>
+<script type="text/javascript">document.getElementById('setStudent').addEventListener('submit', setStudent);</script>
+<?php include '../assets/includes/footer.php'; ?>
